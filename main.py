d518e660f973cb6bbf8f4129c4638d0fab78dd7c
lewis-munyi <lewismunyi97@gmail.com>
2020-12-11 18:56:26 +0300

    Added 404 on error Added tables for list view Added delete url functionality Fixed redirects Code clean up

--- main.py
+++ main.py
@@ -19,19 +19,19 @@ ma = Marshmallow(app)
 
 # URL Class/Model
 class Url(db.Model):
-  code = db.Column(db.String(100), primary_key=True)
-  url = db.Column(db.String(200), unique=True)
-  date = db.Column(db.Date())
+    code = db.Column(db.String(100), primary_key=True)
+    url = db.Column(db.String(200), unique=True)
+    date = db.Column(db.Date())
 
-  def __init__(self, code, url, date):
-    self.code = code
-    self.url = url
-    self.date = date
+    def __init__(self, code, url, date):
+        self.code = code
+        self.url = url
+        self.date = date
 
 # URL Schema
 class UrlSchema(ma.Schema):
-  class Meta:
-    fields = ('code', 'url')
+    class Meta:
+        fields = ('code', 'url')
 
 # Init schema
 url_schema = UrlSchema()
@@ -44,58 +44,67 @@ def load_home():
 # Create a URL
 @app.route('/', methods=['POST'])
 def add_url():
-#   url = request.json['url']
-  url = request.form['url']
+    try:
+        if( request.form['delete']):
+            code = request.form['url']
+            url = Url.query.get(code)
+            db.session.delete(url)
+            db.session.commit()
+            # new = Url.query.all()
+            # return url_schema.jsonify(url)
+            # return jsonify("Delete")
+            result = urls_schema.dump(Url.query.all())
+            return render_template("table.html", payload = result)
+    except:
+        url = request.form['url']
 
-  if(not bool(urlparse(url).scheme)):
-    return render_template('index.html', payload = "Error")
+        if(not bool(urlparse(url).scheme)):
+            return render_template('index.html', payload = "Error")
 
-  for link in Url.query.all():
-    if(link.url == quote_plus(url)):
-      return render_template('index.html', payload = link)
+        for link in Url.query.all():
+            if(link.url == url):
+                return render_template('index.html', payload = link)
 
-   # Generate unique identifier
-  loop = True
-  while(loop):
-      code = str(uuid4())[:6]
-      exists = Url.query.get(code)
+        # Generate unique identifier
+        loop = True
+        while(loop):
+            code = str(uuid4())[:6]
+            exists = Url.query.get(code)
 
-      if(exists == None):
-        loop = False
+            if(exists == None):
+                loop = False
 
-  new_url = Url(code, quote_plus(url), datetime.now())
+        new_url = Url(code, url, datetime.now())
+        db.session.add(new_url)
+        db.session.commit()
 
-  db.session.add(new_url)
-  db.session.commit()
+        return render_template('index.html', payload = new_url)
 
-#   x = url_schema.jsonify(new_url)
-
-  return render_template('index.html', payload = new_url)
-
-#   return url_schema.jsonify(new_url)
 
 # Get Single URL
 @app.route('/<code>', methods=['GET'])
 def get_url(code):
-  # Fetch all URLs
-  if(code == "urls"):
-    all_urls = Url.query.all()
-    result = urls_schema.dump(all_urls)
-    return jsonify(result)
+    # Fetch all URLs
+    if(code == "urls"):
+        all_urls = Url.query.all()
+        result = urls_schema.dump(all_urls)
+        return render_template("table.html", payload = result)
 
-  # Redirect to link
-  url = Url.query.get(code)
-  url = url_schema.dump(url)
-#   return url_schema.jsonify(url)
-  return redirect(unquote(url["url"]), code=200)
+    # Redirect to link
+    url = Url.query.get(code)
+    if(url is None):
+        return render_template('404.html')
+    url = url_schema.dump(url)
+    return redirect(url["url"])
 
 # Delete Short URL
-@app.route('/<code>', methods=['DELETE'])
-def delete_url(code):
-  url = Url.query.get(code)
-  db.session.delete(url)
-  db.session.commit()
-  return url_schema.jsonify(url)
+@app.route('/', methods=['DELETE'])
+def delete_url():
+    code = request.form['url']
+    url = Url.query.get(code)
+    db.session.delete(url)
+    db.session.commit()
+    return url_schema.jsonify(url)
 
 # Run Server
 if __name__ == '__main__':
